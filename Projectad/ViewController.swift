16240d9a2b866c40d0ca83904f66dc12a3d299d2
Eric Rosas <eric@empireappdesignz.com>
2018-11-17 10:26:24 -0500

    Initial Commit

---
+++ Projectad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Projectad
+//
+//  Created by Eric Rosas on 11/17/18.
+//  Copyright © 2018 Eric Rosas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
